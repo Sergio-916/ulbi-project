bad0098ea499d824df2c29298528b653a1591670
Sergey Shpak <sergey.shpak79@gmail.com>
2023-10-06 02:32:45 +0300

    first project

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Counter from "./components/counter";
+import ClassCounter from "./components/ClassCounter";
+import './Styles/App.css'
+import PostItem from "./components/PostItem";
+
+function App() {
+
+  const [posts, setPosts] = useState([
+  { id: 1, title: 'Javascript', body: 'Description' },
+  { id: 2, title: 'Javascript 2', body: 'Description' },
+  { id: 3, title: 'Javascript 3', body: 'Description' },
+  ])
+
+
+  return (
+    <div className="App">
+
+    <h1 style={{textAlign: 'center'}}>
+      Список постов
+    </h1>
+      {/* <PostItem value={'222'} item={{title:0}} number={21}/> */}
+      {/* <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}} />  */}
+
+      {posts.map((post)=>
+        <PostItem post={post} key={post.id}/>
+      )}
+
+
+      {/* <PostItem/> */}
+
+    </div>
+  );
+}
+
+export default App;
