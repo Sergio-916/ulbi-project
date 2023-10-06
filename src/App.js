b3f8f80f488fcd92bb0592c56818614e5ccf8836
Sergey Shpak <sergey.shpak79@gmail.com>
2023-10-06 23:55:25 +0300

    delete items

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ function App() {
   const [posts, setPosts] = useState([
   { id: 1, title: 'Javascript', body: 'Description' },
   { id: 2, title: 'Javascript 2', body: 'Description' },
-  { id: 3, title: 'Javascript 3', body: 'Description' },
-  { id: 4, title: 'Javascript 4', body: 'Description' },
-  { id: 5, title: 'Javascript 5', body: 'Description' },
-  { id: 6, title: 'Javascript 6', body: 'Description' },
-  ])
-
 
   return (
     <div className="App">
